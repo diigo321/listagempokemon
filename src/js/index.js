f207539e4d78359c16d4a7f010be868adaa3426b
Rodrigo Gonçalves <132224602+diigo321@users.noreply.github.com>
2023-05-13 23:12:07 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const botaoAlterarTema = document.getElementById('botao-alterar-tema');
+const body = document.querySelector('body');
+const imagemBotaoTrocaTema = document.querySelector('.imagem-botao');
+
+botaoAlterarTema.addEventListener('click', () => {
+    const modoEscuroEstaAtivo = body.classList.contains('modo-escuro');
+
+    body.classList.toggle('modo-escuro');
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaTema.setAttribute('src', './src/imagens/imagens/sun.png');
+    } else {        
+        imagemBotaoTrocaTema.setAttribute('src', './src/imagens/imagens/moon.png');
+    }       
+
+})
